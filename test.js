297ed7afc3a280821ab6c1f1dccc286453ea808f
Delaney Caceres <dcaceres4@uchicago.edu>
2025-07-21 19:07:12 -0500

    go slack go

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ export const options = {
 
 // Simulated user behavior
 export default function () {
-  let res = http.get("http:loaclhost:5000/"); // Corrected URL to localhost
+  let res = http.get("http://localhost:80/"); // Corrected URL to localhost
   // Validate response status
   check(res, { "status was 200": (r) => r.status == 200 });
   sleep(1);
